5d26b78f0c52d1068254b72f0b02c41e8351a127
Leon Starr <leon_starr@modelint.com>
2025-08-07 07:42:04 -0700

    added test for decision signal

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -2,8 +2,8 @@
 
 import pytest
 from scrall.parse.parser import ScrallParser
-from scrall.parse.visitor import Execution_Unit_a, Signal_a, Signal_Dest_a, N_a, INST_a, PATH_a, R_a, \
-    Seq_Statement_Set_a, Supplied_Parameter_a, External_Signal_a
+from scrall.parse.visitor import (Execution_Unit_a, Signal_a, Signal_Dest_a, N_a, INST_a, PATH_a, R_a,
+    Seq_Statement_Set_a, Supplied_Parameter_a, External_Signal_a, Decision_a, Comp_Statement_Set_a)
 
 actions = [
     ("Time to close ->* me",
@@ -52,6 +52,26 @@ actions = [
                 ),
                 block=None),
             output_token=None)
+     ),
+    ("destination aslev?\n"
+     "    Transfer created -> : No destination -> me",
+     Execution_Unit_a(
+         statement_set=Seq_Statement_Set_a(
+             input_tokens=[],
+             statement=Decision_a(
+                 input=N_a(name='destination aslev'),
+                 true_result=Comp_Statement_Set_a(
+                     statement=Signal_a(
+                         event='Transfer created',
+                         supplied_params=[], dest=None), block=None),
+                 false_result=Comp_Statement_Set_a(
+                     statement=Signal_a(
+                         event='No destination',
+                         supplied_params=[],
+                         dest=Signal_Dest_a(
+                             target_iset=N_a(name='me'), assigner_dest=None, delay=0, cancel=False
+                         )), block=None
+                 )), block=None), output_token=None)
      ),
 ]
 
